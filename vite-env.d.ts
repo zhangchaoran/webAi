564b270eaa560ef1606d94ec128d02cd5c4a6c36
张超然 <15601191153@163.com>
2026-05-13 02:24:25 +0800

    修复 Vercel 构建错误

--- vite-env.d.ts
+++ vite-env.d.ts
@@ -1,5 +1,10 @@
-// src/vite-env.d.ts
-// <reference types="vite/client" />
+/// <reference types="vite/client" />
+
+declare module '*.vue' {
+  import type { DefineComponent } from 'vue'
+  const component: DefineComponent<{}, {}, any>
+  export default component
+}
 
 interface ImportMetaEnv {
   readonly VITE_AI_API_KEY: string
