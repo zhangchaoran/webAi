2f0a403f8ec7487633cf1203b82ee097f2092b3d
张超然 <15601191153@163.com>
2026-05-13 01:45:44 +0800

    d.ts

--- vite-env.d.ts
+++ vite-env.d.ts
@@ -1,11 +1,10 @@
-// src/vite-env.d.ts 添加类型声明
-/// <reference types="vite/client" />
+// src/vite-env.d.ts
+// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-    readonly VITE_AI_API_KEY: string
-    // 添加其他环境变量...
-  }
-  
-  interface ImportMeta {
-    readonly env: ImportMetaEnv
-  }
+  readonly VITE_AI_API_KEY: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
